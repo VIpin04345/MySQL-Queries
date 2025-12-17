f82712a5608fa574fa57f198816da0c427d99027
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-12-17 13:47:20 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -145,7 +145,7 @@ select * from student where city not in ('delhi','noida');
 select * from student limit 3;
 select * from student where marks>70 limit 3;
 
--- select * from student order by marks asc;
+select * from student order by marks asc;
 -- select * from student order by name desc;
 -- select * from student order by name asc limit 4;
 
