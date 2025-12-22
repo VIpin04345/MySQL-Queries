b363944affb8ded93e46f12846d9a37dbac332f2
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-12-22 13:32:47 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -238,13 +238,13 @@ insert into dept values(101,'english'),(102,'it');
 update dept
 set id=103
 where id=102;
--- create table teacher(
--- id int primary key,
--- name varchar(50),
--- dept_id int not null,
--- foreign key (dept_id) references dept(id)
--- on update cascade
--- on delete cascade
+create table teacher(
+id int primary key,
+name varchar(50),
+dept_id int not null,
+foreign key (dept_id) references dept(id)
+on update cascade
+on delete cascade
 
 -- );
 -- insert into teacher values(101,'adam',101),(102,'eve',102);
