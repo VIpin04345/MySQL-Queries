de605dfb783ba2ea40a1a4721e4fa3de444019b1
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2025-12-27 13:54:55 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -435,8 +435,8 @@ SELECT * FROM studentss;
 SELECT AVG(marks)
 FROM studentss;
 
--- SELECT name FROM studentss
--- WHERE marks>87.6667;
+SELECT name FROM studentss
+WHERE marks>87.6667;
 
 -- SELECT name, marks
 -- FROM studentss
