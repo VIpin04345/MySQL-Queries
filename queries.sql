30bb46c6780a4f91f747a8488e0287f2ff10f6f4
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-01-06 13:56:56 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -624,8 +624,8 @@ SELECT * FROM employees
 WHERE name LIKE 'A%';
 SELECT * FROM employees
 WHERE salary BETWEEN 40000 AND 60000;
--- SELECT * FROM employees
--- WHERE city IN ('Pune', 'Delhi');
+SELECT * FROM employees
+WHERE city IN ('Pune', 'Delhi');
 -- UPDATE employees
 -- SET salary = 35000
 -- WHERE emp_id = 5;
