0c9c03dbea67e62b549c8fc2a4126e90b792587e
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-01-25 16:54:47 +0530

    Update interview_sql.sql

--- interview_sql.sql
+++ interview_sql.sql
@@ -3,3 +3,6 @@ SELECT * FROM emp;
 
 -- 2. Display only employee name and salary
 SELECT name, salary FROM emp;
+
+-- 3. Find employees whose salary is greater than 30000
+SELECT * FROM emp WHERE salary > 30000;
