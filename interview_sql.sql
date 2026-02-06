52a813293bc5278a2f019b5b9c2cd397c47d009f
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-06 16:25:06 +0530

    Update interview_sql.sql

--- interview_sql.sql
+++ interview_sql.sql
@@ -268,10 +268,10 @@ WHERE salary > 30000
 GROUP BY dept_id;
 
 -- -- HAVING works after GROUP BY
--- SELECT dept_id, COUNT(*)
--- FROM employees
--- GROUP BY dept_id
--- HAVING COUNT(*) > 5;
+SELECT dept_id, COUNT(*)
+FROM employees
+GROUP BY dept_id
+HAVING COUNT(*) > 5;
 
 
 -- 📌 Interview must-know
