882d61864eed54fe98a8955f27f4182892734c38
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-06 16:27:21 +0530

    Update interview_sql.sql

--- interview_sql.sql
+++ interview_sql.sql
@@ -288,6 +288,6 @@ SELECT salary
 FROM employees e1
 WHERE 1 = (
   SELECT COUNT(DISTINCT salary)
---   FROM employees e2
---   WHERE e2.salary >= e1.salary
+  FROM employees e2
+  WHERE e2.salary >= e1.salary
 -- );
