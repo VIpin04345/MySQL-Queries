d9ab4a6de8e7363f79e947e2cdb52f37274afc35
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-02-13 14:34:07 +0530

    Update interview_sql.sql

--- interview_sql.sql
+++ interview_sql.sql
@@ -304,3 +304,15 @@ CREATE TABLE Employee (
     FOREIGN KEY (DepartmentId) REFERENCES Department(Id)
 );
 
+
+
+INSERT INTO Department VALUES
+(1,'IT'),
+(2,'Sales');
+
+INSERT INTO Employee VALUES
+(1,'Joe',70000,1),
+(2,'Jim',90000,1),
+(3,'Henry',80000,2),
+(4,'Sam',60000,2),
+(5,'Max',90000,1);
