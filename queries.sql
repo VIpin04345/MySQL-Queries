5090e37978d94666eb821734178bed9ac17111d9
Vipin Yadav <124406842+VIpin04345@users.noreply.github.com>
2026-04-25 16:11:50 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -344,6 +344,11 @@ FROM students as a
 LEFT JOIN course as b
 ON a.student_id=b.course_id;
 
+SELECT *
+FROM students as a
+LEFT JOIN course as b
+ON a.student_id=b.course_id;
+
 -- -- RIGHT JOINS------------
 
 SELECT *
